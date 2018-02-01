9b3d06383b48f748c4216f2295b4e7e8851f8cc1
Iti Tyagi <intechworx@gmail.com>
2018-02-01 10:18:23 +0530

    Create Triangle.cs

---
+++ Triangle.cs
@@ -0,0 +1,11 @@
+    static int triangle(int a, int b, int c)
+    {
+        if (a <= 0 || b <= 0 || c <= 0)
+            return 0;
+        else if (a == b && b == c)
+            return 1;
+        else if (a + b > c && a + c > b && b + c > a)
+            return 2;
+        else
+            return 0;
+    }
